8d551e57fd81ccbf3fca7bdc8cbe07374cbce583
jc465871 <rowan.smith2@my.jcu.edu.au>
2018-03-22 16:24:36 +1000

    Refactor to fucntions

--- prac_03 (Week 4)/odd_name.py
+++ prac_03 (Week 4)/odd_name.py
@@ -1,9 +1,17 @@
 """Rowan"""
 
-user_name = input("Please enter your name: ")
-while len(user_name) < 1:
-    print("Name is less than 1 character!")
-    user_name = input("Please enter your name: ")
 
-for x in range(1, len(user_name), 2):
-    print(user_name[x])
+def get_name():
+    user_name = input("Please enter your name: ")
+    while len(user_name) < 1:
+        print("Name is less than 1 character!")
+        user_name = input("Please enter your name: ")
+
+    print_name(user_name)
+
+
+def print_name(user_name, letters=2):
+    for x in range(0, len(user_name), letters):
+        print(user_name[x])
+
+get_name()
