7622785dab4d7ec718122c593745be4bfe73ce6c
rowan-smith <rowan.smith2@my.jcu.edu.au>
2018-04-10 14:10:17 +1000

    Add broken_score

---
+++ prac_01/broken_score.py
@@ -0,0 +1,17 @@
+"""
+CP1404/CP5632 - Practical
+Broken program to determine score status
+"""
+
+score = float(input("Enter score: "))
+if score < 0:
+    print("Invalid score")
+else:
+    if score > 100:
+        print("Invalid score")
+    if score > 50:
+        print("Passable")
+    if score > 90:
+        print("Excellent")
+if score < 50:
+    print("Bad")
