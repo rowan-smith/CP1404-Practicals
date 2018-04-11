e80aadf48507a06887ead824bff9746bcdeaaa14
rowan-smith <rowan.smith2@my.jcu.edu.au>
2018-04-11 11:23:03 +1000

    Bring dictionary in-line with PEP-8 standard

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -5,12 +5,12 @@ File needs reformatting
 """
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
-STATE_NAMES = {"QLD":"Queensland",
+STATE_NAMES = {"QLD": "Queensland",
                "NSW": "New South Wales",
-               "NT" :"Northern Territory",
-               "WA" : "Western Australia",
-               "ACT":"Australian Capital Territory",
-               "VIC":"Victoria", "TAS":"Tasmania"}
+               "NT": "Northern Territory",
+               "WA": "Western Australia",
+               "ACT": "Australian Capital Territory",
+               "VIC": "Victoria", "TAS": "Tasmania"}
 
 
 def main():
