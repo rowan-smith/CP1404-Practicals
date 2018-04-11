688598a39fdbb28bc6f8a96dd1ff2638a79fd355
rowan-smith <rowan.smith2@my.jcu.edu.au>
2018-04-11 14:50:14 +1000

    Error Handling that doesn't work

--- prac_06/convert_miles_to_km.py
+++ prac_06/convert_miles_to_km.py
@@ -13,13 +13,26 @@ class ConverterApp(App):
         return self.root
 
     def handle_increment(self, value):
-        mile_value = int(self.root.ids.input_label.text)
+        mile_value = int(numerical_error_handle(self.root.ids.input_label.text))
         mile_value += int(value)
         self.root.ids.input_label.text = str(mile_value)
 
     def convert_miles_to_kilometres(self):
-        mile_value = int(self.root.ids.input_label.text)
+        try:
+            mile_value = int(self.root.ids.input_label.text)
+        except ValueError:
+            mile_value = int(numerical_error_handle(self.root.ids.input_label.text))
         kilometres = mile_value * MILES_TO_KM
         self.root.ids.output_label.text = "{0:.3f}".format(kilometres)
+
+
+def numerical_error_handle(number):
+    try:
+        if int(number):
+            if int(number) < 1:
+                return int(0)
+            return int(number)
+    except ValueError:
+        return int(0)
 
 ConverterApp().run()
