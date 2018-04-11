b3cc22748ddcd83326e76f953572550451d7d9f6
rowan-smith <rowan.smith2@my.jcu.edu.au>
2018-04-11 11:21:59 +1000

    Add main function

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -5,13 +5,23 @@ File needs reformatting
 """
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
-STATE_NAMES = {"QLD":"Queensland", "NSW": "New South Wales", "NT" :"Northern Territory", "WA" : "Western Australia", "ACT":"Australian Capital Territory", "VIC":"Victoria", "TAS":"Tasmania"}
-# print(STATE_NAMES)
+STATE_NAMES = {"QLD":"Queensland",
+               "NSW": "New South Wales",
+               "NT" :"Northern Territory",
+               "WA" : "Western Australia",
+               "ACT":"Australian Capital Territory",
+               "VIC":"Victoria", "TAS":"Tasmania"}
 
-state = input("Enter short state: ")
-while state != "":
-    if state in STATE_NAMES:
-        print(state, "is", STATE_NAMES[state])
-    else:
-        print("Invalid short state")
+
+def main():
+    # print(STATE_NAMES)
     state = input("Enter short state: ")
+    while state != "":
+        if state in STATE_NAMES:
+            print(state, "is", STATE_NAMES[state])
+        else:
+            print("Invalid short state")
+        state = input("Enter short state: ")
+
+
+main()
