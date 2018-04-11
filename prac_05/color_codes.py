faaac8043a89077c8acfb3ad8f2e10ed1013b70e
rowan-smith <rowan.smith2@my.jcu.edu.au>
2018-04-11 11:42:33 +1000

    Initial Commit

---
+++ prac_05/color_codes.py
@@ -0,0 +1,21 @@
+"""
+CP1401 Hexadecimal Colour Codes
+"""
+
+COLOR_CODES = {"DeepSkyBlue1": "#00bfff",
+               "LawnGreen": "#7cfc00",
+               "DarkOrange": "#ff8c00",
+               "DarkOrchid": "#9932cc",
+               "DeepPink1": "#ff1493",
+               "FloralWhite": "#fffaf0",
+               "magenta": "#ff00ff",
+               "DarkSlateGray4": "#528b8b",
+               "DarkGoldenrod1": "#ffb90f",
+               "chartreuse1": "#7fff00"}
+
+
+def main():
+    pass
+
+
+main()
