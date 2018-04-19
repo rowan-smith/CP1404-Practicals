f9f9c675a56ef9a62dff7327cefabcd2f05e75df
jc465871 <rowan.smith2@my.jcu.edu.au>
2018-04-19 13:56:36 +1000

    Add dynamic check and __str__ to programing_language

---
+++ prac_07/programming_language.py
@@ -0,0 +1,19 @@
+class ProgrammingLanguage:
+
+    def __init__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        string = "{}, {} Typing, Reflection={}, First appeared in {}".format(self.name,
+                                                                             self.typing,
+                                                                             self.reflection,
+                                                                             self.year)
+        return string
+
+    def is_dynamic(self):
+        if self.typing == "Dynamic":
+            return True
+        return False
