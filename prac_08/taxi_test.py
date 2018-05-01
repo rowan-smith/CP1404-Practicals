6eb8bd565fe6ad8f8861b030a36a646ace5fd96a
Rono <rowan.smith2@my.jcu.edu.au>
2018-05-01 12:46:28 +1000

    Update get fare to round

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -1,17 +1,15 @@
 from prac_08.taxi import Taxi
-from prac_08.unreliable_car import UnreliableCar
 
 
 def main():
-    uber = Taxi('Prius 1', 100)
-    uber.drive(40)
-    print(uber)
-    print("${}".format(uber.get_fare()))
-    uber.start_fare()
-    uber.drive(100)
-    print(uber)
-    print("${}".format(uber.get_fare()))
-
+    taxi1 = Taxi('Prius 1', 100)
+    taxi1.drive(40)
+    print(taxi1)
+    print("${}".format(taxi1.get_fare()))
+    taxi1.start_fare()
+    taxi1.drive(100)
+    print(taxi1)
+    print("${}".format(taxi1.get_fare()))
 
 if __name__ == '__main__':
     main()
