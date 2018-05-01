39597ed000942ffbe4fb0911136f13b5d089780e
Rono <rowan.smith2@my.jcu.edu.au>
2018-05-01 13:41:41 +1000

    Finish taxi simulator

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -16,6 +16,8 @@ def main():
         elif user_choice == "D":
             bill_total = drive_taxi(taxis, taxi_choice, bill_total)
         user_choice = input(MENU_TEXT).upper()
+    print("Total trip cost ${:.2f}".format(bill_total))
+    print_taxis(taxis)
 
 
 def drive_taxi(taxis, choice, bill_total):
@@ -28,11 +30,15 @@ def drive_taxi(taxis, choice, bill_total):
 
 
 def choose_taxi(taxis, bill_total):
-    for i, taxi in enumerate(taxis):
-        print("{} - {}".format(i, taxi))
+    print_taxis(taxis)
     chosen_taxi = int(input("Choose taxi: "))
     print("Bill to date: ${:.2f}".format(bill_total))
     return chosen_taxi
+
+
+def print_taxis(taxis):
+    for i, taxi in enumerate(taxis):
+        print("{} - {}".format(i, taxi))
 
 
 if __name__ == '__main__':
